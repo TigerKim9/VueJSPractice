e514bd1e065884c0029cf3f137ca08fe64941a2a
TigerKim9 <kkm9499@gmail.com>
2022-01-19 00:06:07 +0900

    폴더수정

---
+++ vuepractice5/src/App.vue
@@ -0,0 +1,27 @@
+<template><!-- 여기에 HTML-->
+  <img alt="Vue logo" src="./assets/logo.png">
+  내용을 쳐준다.
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script> //여기에  JS
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {hkghkgkgj
+    HelloWorld
+  }
+}
+</script>
+
+<style>/* 여기다 CSS*/
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
